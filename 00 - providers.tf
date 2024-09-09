df2d660d60aeb378b31f3610d7bf54b4c71d054c
Aaron Evans <aarone@outlook.com.au>
2024-09-09 14:16:18 +1000

    chore: comment out unused authentication variables in providers.tf

--- 00 - providers.tf
+++ 00 - providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "boundary" {
   addr                   = var.boundary_address
-  auth_method_id         = var.service_account_authmethod_id
-  auth_method_login_name = var.service_account_name
-  auth_method_password   = var.service_account_password
+  #auth_method_id         = var.service_account_authmethod_id
+  #auth_method_login_name = var.service_account_name
+  #auth_method_password   = var.service_account_password
 }
