3ad7f7eb6173d9e7103aa184c540cb4bddac9e00
Aaron Evans <aarone@outlook.com.au>
2024-09-09 15:20:06 +1000

    chore: Update providers.tf and variables.tf

--- 00 - providers.tf
+++ 00 - providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "boundary" {
-  addr                   = var.boundary_address
+  addr = var.boundary_address
   #auth_method_id         = var.service_account_authmethod_id
   #auth_method_login_name = var.service_account_name
   #auth_method_password   = var.service_account_password
--- 10 - main.tf
+++ 10 - main.tf
@@ -3,49 +3,56 @@ data "boundary_scope" "org" {
   name     = "tfo_apj_demos"
 }
 
-data "boundary_scope" "this" {
+data "boundary_scope" "project_scope" {
   scope_id = data.boundary_scope.org.id
   name     = "shared_services"
 }
 
+
 resource "boundary_host_catalog_static" "this" {
-  name        = "Web UIs"
+  name        = "Allowed Website access (via Transparent Session)"
   description = "A set of web interfaces for SEs to access."
-  scope_id    = data.boundary_scope.this.id
+  scope_id    = data.boundary_scope.project_scope.id
 }
 
+# Generate a Vault token for use in Boundary with a 2-hour renewal period.
+# The token is used with policies for reading LDAP credentials and revoking leases.
 resource "vault_token" "this" {
   no_parent = true
   period    = "2h"
   policies = [
-    "ldap_reader",
-    "revoke_lease"
+    "ldap_reader", # Policy to read LDAP credentials
+    "revoke_lease" # Policy to revoke leases
   ]
 }
 
 module "target" {
   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
-  version = "1.0.11-alpha"
-  # insert required variables here
-  project_name = "shared_services"
+  version = "~> 1"
+
+  project_name    = "shared_services"
   host_catalog_id = boundary_host_catalog_static.this.id
-  hostname_prefix = "ui"
+  hostname_prefix = "GCVE VMware Access"
+
   hosts = [{
-    hostname = "vcenter"
-    address = "10.10.0.6"
-  },{
-    hostname = "nsx"
-    address = "10.10.0.11"
+    hostname = "VMware vCenter"
+    address  = "10.10.0.6"
+    }, {
+    hostname = "VMware NSX"
+    address  = "10.10.0.11"
   }]
+
   services = [{
-    type = "tcp"
-    name = "http"
-    port = 443
+    type             = "tcp"
+    name             = "http"
+    port             = 443
     credential_paths = ["ldap/creds/vsphere_access"]
   }]
+
   credential_store_token = vault_token.this.client_token
-  vault_address = "https://vault.hashicorp.local:8200"
+  vault_address          = "https://vault.hashicorp.local:8200"
 }
+
 
 # resource "boundary_alias_target" "this" {
 #   name                      = "example_alias_target"
