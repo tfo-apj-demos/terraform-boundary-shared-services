1bf9813eee4730707b557b9023b5b9c2fa9c7578
Aaron Evans <aarone@outlook.com.au>
2024-09-11 13:27:56 +1000

    chore: Add debug output for target maps
    Added debug output for vcenter_target, vault_target, and nsx_target modules in the outputs.tf file.

---
+++ 99 - outputs.tf
@@ -0,0 +1,11 @@
+output "vcenter_target_map_debug" {
+  value = module.vcenter_target.target_map_debug
+}
+
+output "vault_target_map_debug" {
+  value = module.vault_target.target_map_debug
+}
+
+output "nsx_target_map_debug" {
+  value = module.nsx_target.target_map_debug
+}
