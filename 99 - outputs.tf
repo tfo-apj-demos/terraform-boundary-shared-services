ec5b66e02e2913be3f2c25f08a424da8ea3093d2
Aaron Evans <aarone@outlook.com.au>
2024-09-11 13:32:21 +1000

    chore: Update target map debug output in outputs.tf

--- 99 - outputs.tf
+++ 99 - outputs.tf
@@ -1,11 +1,11 @@
 output "vcenter_target_map_debug" {
-  value = module.vcenter_target.target_map_debug
+  value = module.vcenter_target.vcenter_target_map_debug
 }
 
 output "vault_target_map_debug" {
-  value = module.vault_target.target_map_debug
+  value = module.vault_target.vault_target_map_debug
 }
 
 output "nsx_target_map_debug" {
-  value = module.nsx_target.target_map_debug
+  value = module.nsx_target.nsx_target_map_debug
 }
