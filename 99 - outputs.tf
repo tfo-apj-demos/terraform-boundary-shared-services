da1b5f2b5bb60a9508eb7a0357e5cf9c00d2ef23
Aaron Evans <aarone@outlook.com.au>
2024-09-17 22:41:33 +1000

    chore: Add debug output mappings for vCenter, NSX, and Vault targets in outputs.tf

---
+++ 99 - outputs.tf
@@ -0,0 +1,23 @@
+output "vcenter_services_map_debug" {
+  value = module.vcenter_target.services_map_debug
+}
+
+output "vcenter_target_map_debug" {
+  value = module.vcenter_target.target_map_debug
+}
+
+output "nsx_services_map_debug" {
+  value = module.nsx_target.services_map_debug
+}
+
+output "nsx_target_map_debug" {
+  value = module.nsx_target.target_map_debug
+}
+
+output "vault_services_map_debug" {
+  value = module.vault_target.services_map_debug
+}
+
+output "vault_target_map_debug" {
+  value = module.vault_target.target_map_debug
+}
