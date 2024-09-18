2a441f97de084024d8051e8103276d05c4664d77
Aaron Evans <aarone@outlook.com.au>
2024-09-18 22:03:53 +1000

    chore: Add output mappings for NSX target credentials in outputs.tf

---
+++ 99 - outputs.tf
@@ -0,0 +1,11 @@
+output "tcp_with_creds_target_ids" {
+    value = moddule.nsx_target.tcp_with_creds_target_ids
+}
+
+output "ssh_with_creds_target_ids" {
+    value = module.nsx_target.ssh_with_creds_target_ids
+}
+
+output "alias_destination_ids" {
+    value = module.nsx_target.alias_destination_ids
+}
