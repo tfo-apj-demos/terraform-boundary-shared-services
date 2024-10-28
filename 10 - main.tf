14b6f3c25351797e990366094d9741118b2ec8c2
Aaron Evans <aarone@outlook.com.au>
2024-10-29 07:02:30 +1100

    chore: Remove output for vault_token to enhance security

--- 10 - main.tf
+++ 10 - main.tf
@@ -9,9 +9,6 @@ resource "vault_token" "this" {
   ]
 }
 
-output "vault_token" {
-  value = nonsensitive(vault_token.this.client_token)
-}
 
 module "vault_server_target" {
   source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
