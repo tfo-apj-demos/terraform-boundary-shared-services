6a1f82cfa0f86b01940c9ce93cdf0e37bf54c1db
Aaron Evans <aarone@outlook.com.au>
2024-10-29 06:31:32 +1100

    chore: Add windows_remote_desktop_target module for Windows Remote Desktop Server configuration

--- 10 - main.tf
+++ 10 - main.tf
@@ -11,12 +11,12 @@ resource "vault_token" "this" {
 
 module "vault_server_target" {
   source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
+
   project_name         = "shared_services"
   target_name          = "Vault Server Access"
   hosts                = ["vault.hashicorp.local"]
   port                 = 8200
   target_type          = "tcp"
-  target_mode          = "single"
 
   # Vault credential configurations
   use_credentials      = false
@@ -27,6 +27,7 @@ module "vault_server_target" {
 
 module "vcenter_target" {
   source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
+
   project_name         = "shared_services"
   target_name          = "vCenter Server Access"
   hosts                = ["vcsa-98975.fe9dbbb3.asia-southeast1.gve.goog"]
@@ -46,12 +47,12 @@ module "vcenter_target" {
 
 module "nsx_server_target" {
   source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
+
   project_name         = "shared_services"
   target_name          = "NSX Server Access"
   hosts                = ["nsx-98984.fe9dbbb3.asia-southeast1.gve.goog"]
   port                 = 443
   target_type          = "tcp"
-  target_mode          = "single"
 
   # Vault credential configurations
   use_credentials      = false
@@ -60,62 +61,25 @@ module "nsx_server_target" {
   alias_name           = "nsx-98984.fe9dbbb3.asia-southeast1.gve.goog"
 }
 
-# module "nsx_target" {
-#   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
-#   version = "~> 2.0.1"
+module "windows_remote_desktop_target" {
+  source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
+  
+  project_name         = "shared_services"
+  target_name          = "Windows Remote Desktop Server"
+  hosts                = ["rds-01.hashicorp.local"]
+  port                 = 3389
+  target_type          = "tcp"
+  
+  # Vault credential configurations
+  use_credentials      = true
+  credential_store_token = vault_token.this.client_token
+  vault_address        = "https://vault.hashicorp.local:8200"
+  credential_source    = "vault"
+  credential_path      = "ldap/creds/vault_ldap_dynamic_demo_role"
+  
+  # Alias name matching one of the Windows servers or a primary address for access
+  alias_name           = "rds-01.hashicorp.local"
 
-#   project_name           = "shared_services"
-#   hostname_prefix        = "On-Prem VMware NSX-T Manager Console"
-
-#   hosts = [{
-#     fqdn  = "nsx-98984.fe9dbbb3.asia-southeast1.gve.goog"
-#   }]
-
-#   services = [{
-#     type               = "tcp"
-#     port               = 443
-#     use_existing_creds = false
-#     use_vault_creds    = false
-#   }]
-# }
-
-# module "vcenter_target" {
-#   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
-#   version = "~> 2.0.1"
-
-#   project_name           = "shared_services"
-#   hostname_prefix        = "On-Prem VMware vCenter Console"
-#   credential_store_token = vault_token.this.client_token
-#   vault_address          = "https://vault.hashicorp.local:8200"
-
-#   hosts = [{
-#     fqdn  = "vcsa-98975.fe9dbbb3.asia-southeast1.gve.goog"
-#   }]
-
-#   services = [{
-#     type               = "tcp"
-#     port               = 443
-#     use_existing_creds = false
-#     use_vault_creds    = true
-#     credential_path    = "ldap/creds/vsphere_access"
-#   }]
-# }
-
-# module "vault_target" {
-#   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
-#   version = "~> 2.0.1"
-
-#   project_name    = "shared_services"
-#   hostname_prefix = "On-Prem HashiCorp Vault Console"
-
-#   hosts = [{
-#     fqdn  = "vault.hashicorp.local"
-#   }]
-
-#   services = [{
-#     type               = "tcp"
-#     port               = 8200
-#     use_existing_creds = false
-#     use_vault_creds    = false
-#   }]
-# }
+  # Optionally specify target_mode if needed
+  target_mode          = "single"  # or "group" if multiple servers should be treated as one target
+}
