ac5a87c031b4f4c8d0d878cd56a6dd68d6c709ff
Aaron Evans <aarone@outlook.com.au>
2024-10-30 23:04:58 +1100

    chore: Consolidate existing credential store reference in Windows Remote Desktop module

--- 10 - main.tf
+++ 10 - main.tf
@@ -77,13 +77,12 @@ module "windows_remote_desktop_target" {
   vault_address        = "https://vault.hashicorp.local:8200"
   credential_source    = "vault"
   credential_path      = "ldap/creds/vault_ldap_dynamic_demo_role"
+  # Reference the existing credential store from vCenter target since it's already created 
+  # and you can't have multiple credential stores for the same Boundary Project
+  existing_credential_store_id = module.vcenter_target.credential_store_id 
   
   # Alias name matching one of the Windows servers or a primary address for access
   alias_name           = "rds-01.hashicorp.local"
-
-  # Reference the existing credential store from vCenter target since it's already created 
-  # and you can't have multiple credential stores for the same project
-  existing_credential_store_id = module.vcenter_target.credential_store_id 
 }
 
 # boundary module for https://aap-aap.apps.openshift-01.hashicorp.local/
